e1efe6f7d4bb181bb365228cd82afdeda59d74ad
madawei2699 <madawei2699@gmail.com>
2019-12-16 17:39:34 +0800

    refactor with module

---
+++ bin/todo-cli.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var glob = require("glob");
+var parse = require('../lib/ast')
+var render = require('../lib/render')
+var argv = require('yargs').argv;
+
+let debug = false;
+let base_dir = "./";
+let out_file = "./todo.html";
+
+var doing_json = {
+    "todo": []
+};
+var critical_json = {
+    "todo": []
+};
+
+try {
+    let files = glob.sync(base_dir + "/**/*.todo");
+    files.forEach(function (f) {
+        var data = fs.readFileSync(f, 'utf8') + "\n";
+
+        if (debug) {
+            lexer.setInput(data);
+            let arr = f.split('/');
+            console.log("\n" + arr[arr.length - 1] + " lex token is:\n");
+            let tokens = [];
+            while (token = lexer.lex()) {
+                if (token === 'NAME') {
+                    token += ": " + lexer.yytext
+                    console.log(token)
+                } else {
+                    console.log(token)
+                }
+                tokens.push(token);
+            }
+        }
+        
+        var out_json = parse(data, 0);
+        if (out_json != null) {
+            doing_json.todo = doing_json.todo.concat(out_json);
+        }
+        out_json = parse(data, 1);
+        if (out_json != null) {
+            critical_json.todo = critical_json.todo.concat(out_json);
+        }
+    });
+
+    var output_index_html = render(doing_json, critical_json);
+    if (out_file != "") {
+        fs.writeFile(out_file, output_index_html, function(err, result) {
+            if (err) throw err;
+        });
+    } else {
+        console.log(output_index_html);
+    }
+} catch(e) {
+    console.log('Error:', e.stack);
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# TODO++ parser #
+
+*.todo -> json -> html (use lex and jison to parse yaml like format txt to json ast).
+
+todo txt format see test/test_data.
+
+## Installation ##
+
+```bash
+npm install -g todo-plus-parser
+```
+
+## Getting Started ##
+
+## parse html with special dir
+
+```bash
+todo-plus-parser -i "./todos/" -o out.html
+```
+
+## enable debug
+
+```bash
+todo-plus-parser -i "./todos/" -o out.html -d true
+```
