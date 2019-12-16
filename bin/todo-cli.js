240406c5f994e7a6a4b2ea03d1f5ea29505c374f
madawei2699 <madawei2699@gmail.com>
2019-12-16 22:09:05 +0800

    fix eslint error

--- bin/todo-cli.js
+++ bin/todo-cli.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var glob = require("glob");
-var parse = require('../lib/ast');
-var lexer = require('../lib/ast').lexer;
-var render = require('../lib/render');
-var argv = require('yargs')
+const fs = require('fs');
+const glob = require('glob');
+const parse = require('../lib/ast');
+const lexer = require('../lib/ast').lexer;
+const render = require('../lib/render');
+const argv = require('yargs')
     .usage('Usage: todo-plus-parser -i path -o out_file')
-    .example('todo-plus-parser -i "./test/test_data" -o "./out.html"', 'parse input dir todo file to render out.html file')
+    .example('todo-plus-parser -i "./test/test_data" -o "./out.html"',
+        'parse input dir todo file to render out.html file')
     .describe('i', 'input dir')
     .describe('o', 'input dir')
     .describe('d', 'debug switch')
@@ -16,56 +17,57 @@ var argv = require('yargs')
     .epilog('bmpidev copyright 2019')
     .argv;
 
-let debug = argv.d;
-let base_dir = argv.i;
-let out_file = argv.o;
+const debug = argv.d;
+const baseDir = argv.i;
+const outFile = argv.o;
 
-var doing_json = {
-    "todo": []
+const doingJson = {
+  'todo': [],
 };
-var critical_json = {
-    "todo": []
+const criticalJson = {
+  'todo': [],
 };
 
 try {
-    let files = glob.sync(base_dir + "/**/*.todo");
-    files.forEach(function (f) {
-        var data = fs.readFileSync(f, 'utf8') + "\n";
+  const files = glob.sync(baseDir + '/**/*.todo');
+  files.forEach(function(f) {
+    const data = fs.readFileSync(f, 'utf8') + '\n';
 
-        if (debug) {
-            lexer.setInput(data);
-            let arr = f.split('/');
-            console.log("\n" + arr[arr.length - 1] + " lex token is:\n");
-            let tokens = [];
-            while (token = lexer.lex()) {
-                if (token === 'NAME') {
-                    token += ": " + lexer.yytext
-                    console.log(token)
-                } else {
-                    console.log(token)
-                }
-                tokens.push(token);
-            }
+    if (debug) {
+      lexer.setInput(data);
+      const arr = f.split('/');
+      console.log('\n' + arr[arr.length - 1] + ' lex token is:\n');
+      const tokens = [];
+      let token;
+      while (token = lexer.lex()) {
+        if (token === 'NAME') {
+          token += ': ' + lexer.yytext;
+          console.log(token);
+        } else {
+          console.log(token);
         }
-        
-        var out_json = parse(data, 0);
-        if (out_json != null) {
-            doing_json.todo = doing_json.todo.concat(out_json);
-        }
-        out_json = parse(data, 1);
-        if (out_json != null) {
-            critical_json.todo = critical_json.todo.concat(out_json);
-        }
-    });
-
-    var output_index_html = render(doing_json, critical_json);
-    if (out_file != "") {
-        fs.writeFile(out_file, output_index_html, function(err, result) {
-            if (err) throw err;
-        });
-    } else {
-        console.log(output_index_html);
+        tokens.push(token);
+      }
     }
-} catch(e) {
-    console.log('Error:', e.stack);
+
+    let outJson = parse(data, 0);
+    if (outJson != null) {
+      doingJson.todo = doingJson.todo.concat(outJson);
+    }
+    outJson = parse(data, 1);
+    if (outJson != null) {
+      criticalJson.todo = criticalJson.todo.concat(outJson);
+    }
+  });
+
+  const outputIndexHtml = render(doingJson, criticalJson);
+  if (outFile != '') {
+    fs.writeFile(outFile, outputIndexHtml, function(err, result) {
+      if (err) throw err;
+    });
+  } else {
+    console.log(outputIndexHtml);
+  }
+} catch (e) {
+  console.log('Error:', e.stack);
 }
